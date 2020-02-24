5c0200bbb62a8ef9b2c69566b971f594ee72d162
ARJUN KUMAR A E <arjunae798@gmail.com>
2020-02-24 19:55:59 +0530

    Update create.sql

--- src/main/java/com/chainsys/create.sql
+++ src/main/java/com/chainsys/create.sql
@@ -1,3 +1,6 @@
+
+create sequence employee_id_seq start with 1 ;
+
 create table bloodGroupDetails(
 blood_group_id number,
 blood_group varchar2(20),
@@ -10,8 +13,8 @@ user_name varchar2(30) not null,
 email varchar2(30) UNIQUE,
 blood_group_id number ,
 active number default 1,
+city varchar2(30),
 constraint fkc_blood foreign key(blood_group_id) references bloodGroupDetails(blood_group_id)
 );
 
 
-create sequence employee_id_seq start with 1 ;
